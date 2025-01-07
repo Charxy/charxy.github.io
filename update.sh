5d5e9a61076add22690d22e8e1992720ace72241
Charxy <1309178583@qq.com>
2025-01-07 09:26:09 +0800

    魔改vivia：add user tag for valine - 20250107092607

--- update.sh
+++ update.sh
@@ -1,12 +1,22 @@
 npx hexo clean # 清除缓存
 npx hexo g && npx hexo d # 重新部署 Hexo
 
-# 如果没有消息后缀，默认提交信息为 `:pencil: update content`
+# 获取当前日期和时间，并格式化为 YYYYMMDDHHMMSS
+datetime=$(date +"%Y%m%d%H%M%S")
+ 
+# 如果没有提供消息后缀，默认提交信息为 ":pencil: update content"
 info=$1
-if ["$info" = ""];
-then info=":pencil: update content"
+if [ "$info" = "" ]; then
+    info=":pencil: update content"
 fi
-
+ 
+# 在 info 后面拼接时间戳信息
+info_with_timestamp="${info} - $datetime"
+ 
+# 备份 Hexo 源文件
 git add -A
-git commit -m "$info"
-git push origin hexo # 备份 Hexo 源文件
+git commit -m "$info_with_timestamp"
+git push origin hexo
+	
+# 打包源代码，备份到本地
+7z a ../Hexo-Blog_deploy-backup/backup_$datetime.7z ./*
